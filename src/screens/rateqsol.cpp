ef8cce94ea233030bf9137feffdfa17f5edcddb6
András El Koulali <deak311@gmail.com>
2024-02-19 22:08:57 +0100

    Final cleanup

--- src/screens/rateqsol.cpp
+++ src/screens/rateqsol.cpp
@@ -5,9 +5,9 @@ namespace screens::rateqsol {
 
 void solvinator(uint8_t eq) {}
 
-menu::menu_item eq_menuitems[] = {{"R = U/I", false, solvinator, 0}, {"F = k * Q1*Q2 / r^2", false, solvinator, 1}};
+const menu::menu_item eq_menuitems[] = {{"R = U/I", false, solvinator, 0}, {"F = k * Q1*Q2 / r^2", false, solvinator, 1}};
 
-zcas::ratio_eq equations[] = {{1, {{cln::cl_symbol{"U"}, 1}, {cln::cl_symbol{"I"}, -1}, {cln::cl_symbol{"R"}, -1}}},
+const zcas::ratio_eq equations[] = {{1, {{cln::cl_symbol{"U"}, 1}, {cln::cl_symbol{"I"}, -1}, {cln::cl_symbol{"R"}, -1}}},
                               {"9000000000", {{cln::cl_symbol{"Q1"}, 1}, {cln::cl_symbol{"Q2"}, 1}, {cln::cl_symbol{"r"}, -2}, {cln::cl_symbol{"F"}, -1}}}};
 
 menu::menu eqs_menu(eq_menuitems, sizeof(eq_menuitems) / sizeof(eq_menuitems[0]));
